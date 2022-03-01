6a204f270d9ef8739735543e3aad1ca00766977a
Kiran Gurung <kg820532@gmail.com>
2022-03-01 14:10:28 +0000

    core

---
+++ src/NameLength.js
@@ -0,0 +1,8 @@
+const NameLength = (props) => {
+  return  <p className="name-length">
+        Did you know that {props.name} is {props.length} characters long?!
+    </p>
+
+}
+
+export default NameLength
